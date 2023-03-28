cde4ef12d1ec4268d6ab12b4c8bd124a829d0de7
Iain McMurray <jedijones314@gmail.com>
2023-03-28 15:49:03 -0700

    added main menu and variable boulder hp

--- asteroids.py
+++ asteroids.py
@@ -24,7 +24,7 @@ class asteroid(object):
 		self.y += dy
 		self.hbox.move_ip(dx, dy)
 	def draw(self, surface):
-		pygame.draw.circle(surface, (100, 100, 100), (self.x, self.y), self.radius)
+		pygame.draw.circle(surface, (100-self.hp*20, 100-self.hp*20, 100-self.hp*20), (self.x, self.y), self.radius)
 class bullet(object):
 	bulletList = []
 	def __init__(self, x, y, d, vel, radius, dmg=1):
@@ -48,10 +48,12 @@ class bullet(object):
 		pygame.draw.circle(surface, (100, 10, 10), (self.x, self.y), self.radius)
 class turret(object):
 	turList = []
-	def __init__(self, x, y, spd):
+	def __init__(self, x, y, spd,hp=5,dmg=1):
 		self.x = x
 		self.y = y
 		self.spd = spd
+		self.hp = hp
+		self.dmg=dmg
 		turret.turList.append(self)
 	def move(self, direction):
 		self.x += self.spd*direction
@@ -61,24 +63,50 @@ class turret(object):
 class Direction(IntEnum):
 	LEFT = -1
 	RIGHT = 1
+def mainMenu(surface):
+	surface.fill((100, 40, 50))
+	lheight = 40
+	lwidth = 200
+	r = pygame.Rect((width-lwidth)/2, (height-lheight)/2, lwidth, lheight)
+	pygame.draw.rect(surface, (200, 200, 200), r)
+	my_font = pygame.font.SysFont('Comic Sans MS', 30)
+	text_surface = my_font.render('Play', False, (50, 20, 50))
+	surface.blit(text_surface, (width/2-lwidth/8,height/2-lheight/1.75))	
+	pygame.display.update()
+	while True:
+		mousepos = pygame.mouse.get_pos()
+		c = pygame.Rect(mousepos[0]-2, mousepos[1]-2, 4, 4)
+		if pygame.mouse.get_pressed()[0]:
+			if pygame.Rect.colliderect(r, c):
+				break
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+		pygame.time.delay(50)
 def collisionCheck():
 	for b in bullet.bulletList:
 		for a in asteroid.astList:
 			if pygame.Rect.colliderect(b.hbox, a.hbox):
 				b.removeself()
-				a.removeself()
 				del b
-				del a
+				a.hp -= t.dmg
+				if a.hp <= 0:
+					a.removeself()
+					del a
 				break
 def degToRad(deg):
 	return deg*-.0174533
 def redrawWindow(surface):
-	surface.fill((0,0,0))
+	surface.fill((20,20,30))
 	[ast.move() for ast in asteroid.astList]
 	[ast.draw(surface) for ast in asteroid.astList]
 	[b.move() for b in bullet.bulletList]
 	[b.draw(surface) for b in bullet.bulletList]
 	[t.draw(surface) for t in turret.turList]
+	hpBkg = pygame.Rect(10, 10, 500, 30)
+	hpBar = pygame.Rect(10, 10, t.hp * 100, 30)
+	pygame.draw.rect(surface, (100,70,80), hpBkg)
+	pygame.draw.rect(surface, (230,10,15), hpBar)
 	pygame.display.update()
 def spawnBullet(spawnX, spawnY):
 	spd = bullSpd
@@ -91,7 +119,8 @@ def spawnAsteroid():
 	randEndX = random.random() * width
 	randSpeed = round(random.random() * 4 + 2)
 	dir = math.atan2(height, -randBegX + randEndX)
-	asteroid(randBegX, 100, dir, randSpeed, 25)
+	randHp = round(random.random()) * 2
+	asteroid(randBegX, 100, dir, randSpeed, 25, randHp)
 def removeOutOfBounds():
 	for b in bullet.bulletList:
 		if b.y < 0:
@@ -101,6 +130,7 @@ def removeOutOfBounds():
 	for a in asteroid.astList:
 		if a.y > height:
 			a.removeself()
+			turret.turList[0].hp -= 1
 			del a
 			break
 def init():
@@ -116,10 +146,14 @@ def init():
 	width = 800
 	bulletSpawnCoords = {"x": width/2, "y":height};
 def main():
-	win = pygame.display.set_mode((width, height))
+	global t
 	t = turret(width/2, height, 5)
+	win = pygame.display.set_mode((width, height))
 	cooldown = 5
 	spawnCoolDown = 30
+	#main menu:
+	mainMenu(win)
+	#game loop:
 	while True:
 		#Kill Switch
 		for event in pygame.event.get():
